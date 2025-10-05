dc8a67bf66c63ea3bf32b3684a633f65e7bb76a4
Lug <145815811+Lug-yng@users.noreply.github.com>
2025-10-05 18:05:07 -0300

    0.1

--- medianotas.js
+++ medianotas.js
@@ -4,13 +4,16 @@
 const n1 = 12; 
 const n2 = 17;
 const n3 = 8;
+
 /*aqui criei uma variavel que somara e guardara a soma das
  três outras variaveis  */
 const soma = n1 + n2 + n3;
+
+const media = soma / 3;
 /* aqui essa variavel vai dividir por tres a soma das 
 variaveis assim a variavem media guardara a media das
 tres notas */
-const media = soma / 3;
+
 console.log("A media é:" , media.toFixed(2));
 /* aqui usei toFixed para deixar as casas decimais em 2
 e em media.toFixed o ponto é usado como uma virgula*/
