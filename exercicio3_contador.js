53c8b247562cbecddc5ab21baa5c2945f9845a2d
Lug <145815811+Lug-yng@users.noreply.github.com>
2025-10-06 21:56:47 -0300

    exercício 3 contador

---
+++ exercicio3_contador.js
@@ -0,0 +1,12 @@
+// Exercício 3 — Contador de 1 a 10
+
+// O loop "for" vai repetir uma ação várias vezes
+let i = 1;
+
+/*enquanto o valor de i for menor ou igual a 10 vai repetir o que esta dentro de {} e a cada
+repetição vai mostrar o valor de i e vai aumentar i em 1 */
+while (i <= 10) {  
+  console.log(`Contagem: ${i}`);
+  i++;
+}
+
