33b545eb15baf6f9898bafc7de7bf65009c64a89
Lug <145815811+Lug-yng@users.noreply.github.com>
2025-10-07 20:28:29 -0300

    Adiciona exercício 4 que  exibe uma mensagem personalizada.

---
+++ exercicio4_saudacao.js
@@ -0,0 +1,11 @@
+// Exercício 4 — Função de saudação
+
+/* 
+Cria uma função chamada "saudar".
+Essa função recebe um nome e exibe uma mensagem de boas-vindas com esse nome.
+*/
+function saudar (nome) {
+    console.log(`olá, ${nome}! seja bem vindo(a) ao programa.`);
+}
+
+saudar ("Lug");
